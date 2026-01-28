422d7f2aa3ad744961b15ea940f903ae9b9e3c15
Soleil Demick <chasedemick@gmail.com>
2026-01-28 12:40:18 -0500

    what the fuck

---
+++ ES28W26/workspace/ADCPot/Debug/Library/mylib/sysinit.d
@@ -0,0 +1,18 @@
+Library/mylib/sysinit.o: \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/mylib/sysinit.c \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Device/stm32c0xx.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Device/stm32c031xx.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/core_cm0plus.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/cmsis_version.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/cmsis_compiler.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/cmsis_gcc.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/mpu_armv7.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Device/system_stm32c0xx.h
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Device/stm32c0xx.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Device/stm32c031xx.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/core_cm0plus.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/cmsis_version.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/cmsis_compiler.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/cmsis_gcc.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/mpu_armv7.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Device/system_stm32c0xx.h:
---
+++ ES28W26/workspace/ADCPot/Debug/Src/ADCPot.d
@@ -0,0 +1,21 @@
+Src/ADCPot.o: ../Src/ADCPot.c \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/mylib/ES28.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Device/stm32c0xx.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Device/stm32c031xx.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/core_cm0plus.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/cmsis_version.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/cmsis_compiler.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/cmsis_gcc.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/mpu_armv7.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Device/system_stm32c0xx.h \
+ /home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/mylib/uart.h
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/mylib/ES28.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Device/stm32c0xx.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Device/stm32c031xx.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/core_cm0plus.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/cmsis_version.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/cmsis_compiler.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/cmsis_gcc.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Include/mpu_armv7.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/chipheaders/Device/system_stm32c0xx.h:
+/home/puppygirl-dirac/Documents/coursework/winter_2026/ES28W26/Library/mylib/uart.h:
